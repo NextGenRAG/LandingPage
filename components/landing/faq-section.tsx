eb561689962869024ab740c91004d395320229ee
Estevan Guerra <estevanguerra8@gmail.com>
2025-03-01 14:46:29 -0600

    imports update

--- components/landing/faq-section.tsx
+++ components/landing/faq-section.tsx
@@ -5,7 +5,8 @@ import {
     AccordionContent,
     AccordionItem,
     AccordionTrigger,
-  } from "@/components/ui/accordion";
+  } from "../../components/ui/accordion";
+  //components\ui\accordion.tsx
    
   const faqs = [
     {
