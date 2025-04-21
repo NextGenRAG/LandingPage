09c3cc0ea52dc1428837b0f2ba292250c9861c82
estevanguerra8 <estevanguerra8@gmail.com>
2025-04-21 15:01:19 -0500

    updated gay

--- components/magicui/interactive-hover-button.tsx
+++ components/magicui/interactive-hover-button.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { cn } from "@/lib/utils";
+import { cn } from "../../lib/utils";
 import { ArrowRightIcon } from "@radix-ui/react-icons";
 
 interface InteractiveHoverButtonProps
