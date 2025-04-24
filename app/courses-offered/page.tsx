d473ec355dbf5fd54dd9afdee787ef1f280a1b13
estevanguerra8 <estevanguerra8@gmail.com>
2025-04-24 16:42:31 -0500

    fixed imports for site header and footer in new courses offered page

--- app/courses-offered/page.tsx
+++ app/courses-offered/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import { SiteHeader } from '@/components/site-header';
-import { SiteFooter } from '@/components/site-footer';
+import { SiteHeader } from '../../components/site-header';
+import { SiteFooter } from '../../components/site-footer';
 import { Search } from 'lucide-react';
 
 interface Course {
